88f7c8c9e8939c05f8a6a1251dd6b19226269d4a
Samarjeet1Singh <adwitiya.sam.singh@gmail.com>
2024-11-17 10:30:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function fadein(){
+document.getElementById("col2").style.animation="fadeIn 5s";
+}
+
+function fadein2(){
+    document.getElementById("col3").style.animation="fadeIn 5s";
+    
+    
+    }
